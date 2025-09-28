b84a21ca8bf0a7f5db736948657e357f66b261c1
Gershom-Benni <gershombenni007@gmail.com>
2025-09-29 01:54:41 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../css_files/Header.css"
+const Header = () => {
+  return (
+    <header>
+        <h1>Team Estrix</h1>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../css_files/Homepage.css"
+import Header from '../components/Header'
+const Homepage = () => {
+  return (
+    <Header></Header>
+  )
+}
+
+export default Homepage
