3860d4b74df9a321aa70b2bc7d57735b3e11d3b7
Gershom-Benni <gershombenni007@gmail.com>
2025-10-02 14:11:30 +0530

    Added Solution Page

--- src/components/Team_members.jsx
+++ src/components/Team_members.jsx
@@ -6,12 +6,13 @@ const Team_members = () => {
     <>
         <h1 className={styles.heading}>Our Team</h1>
         <div className={styles.card_container}>
-            <Team_member_card img_path="prem image.jpg" name="Bagya Prem K" role="Team Leader"></Team_member_card>
-            <Team_member_card img_path="Gershom image.jpg" name="Gershom Benni P" role="Software and IOT"></Team_member_card>
+            <Team_member_card img_path="prem image.jpg" name="Bagya Prem K" role="Team Leader and Hardware"></Team_member_card>
+            <Team_member_card img_path="Gershom image.jpg" name="Gershom Benni P" role="Software"></Team_member_card>
             <Team_member_card img_path="Vikraman Image.png" name="Vikraman S" role="Software"></Team_member_card>
             <Team_member_card img_path="abhijith image.jpeg" name="Abhijith" role="Research and Development"></Team_member_card>
-            <Team_member_card img_path="dhuyukshaa image.jpg" name="Dhuyukshaa A" role="R&D and IOT"></Team_member_card>
             <Team_member_card img_path="no image.jpg" name="Milan R" role="Hardware"></Team_member_card>
+            <Team_member_card img_path="dhuyukshaa image.jpg" name="Dhuyukshaa A" role="R&D and IOT"></Team_member_card>
+            
         </div>
     </>
   )
--- src/pages/Solution.jsx
+++ src/pages/Solution.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import Header from "../components/Header.jsx"
 const Solution = () => {
   return (
-    <div>Solution</div>
+    <>
+      <Header></Header>
+      <h1>Under Development</h1>
+    </>
   )
 }
 
