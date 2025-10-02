f719b78f781be243b521e1296f90a3ebe133634b
Gershom-Benni <gershombenni007@gmail.com>
2025-10-02 14:04:49 +0530

    Added Nav Button to solution page, implemented react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Homepage from "./pages/Homepage"
+import Solution from "./pages/Solution"
+import { Routes, Route, Link } from 'react-router-dom';
 function App() {
 
   return (
     <>
-    <Homepage></Homepage>
+    <Routes>
+      <Route path="/" element={<Homepage></Homepage>}></Route>
+      <Route path="/solution" element={<Solution></Solution>}></Route>
+    </Routes>
     </>
 )
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from "../css_files/Homepage.module.css"
+ import { Routes, Route, Link } from 'react-router-dom';
 import Header from '../components/Header'
 import Team_members from '../components/Team_members.jsx'
 const Homepage = () => {
@@ -7,6 +8,7 @@ const Homepage = () => {
     <>
       <Header></Header>
       <Team_members></Team_members>
+      <Link to="/solution" style={{ textDecoration: "none", color: "inherit" }}><button className={styles.btn}>View Our Solution</button></Link>
     </>
   )
 }
